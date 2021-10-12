e9d523142da3f208682be67d397a25676b3ccdfa
jaabanks <86501922+jaabanks@users.noreply.github.com>
2021-10-12 17:23:54 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "namespace" {
+  description = "The project namespace to use for unique resource naming"
+  type        = string
+}
+
+variable "ssh_keypair" {
+  description = "SSH keypair to use for EC2 instance"
+  default     = null #A
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  default     = "us-west-2"
+  type        = string
+}
