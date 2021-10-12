f739470a1b84d8505174bb980c6473c5bcfbf8c4
jaabanks <86501922+jaabanks@users.noreply.github.com>
2021-10-12 17:32:36 -0500

    Create main.tf

---
+++ modules/database/main.tf
@@ -0,0 +1,19 @@
+resource "random_password" "password" { #A
+  length           = 16
+  special          = true
+  override_special = "_%@/'\""
+}
+
+resource "aws_db_instance" "database" {
+  allocated_storage      = 10
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t2.micro"
+  identifier             = "${var.namespace}-db-instance"
+  name                   = "pets"
+  username               = "admin"
+  password               = random_password.password.result
+  db_subnet_group_name   = var.vpc.database_subnet_group #B
+  vpc_security_group_ids = [var.sg.db] #B
+  skip_final_snapshot    = true
+}
