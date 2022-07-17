2e4b2237b47d4f5dced75d3e00ac355a4b397c3f
Jason Ceballos <30805605+jasonceballos29@users.noreply.github.com>
2022-07-17 00:02:28 -0400

    updating provider files

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region  = var.region
   access_key = "AWS_ACCESS_KEY_ID"
   secret_key = "AWS_SECRET_ACCESS_KEY"
-  # assume_role {
-  # role_arn = "arn:aws:iam::254452634027:role/EC2Creation"
-  # }
+  assume_role {
+  role_arn = "arn:aws:iam::254452634027:role/EC2Creation"
+  }
 }
