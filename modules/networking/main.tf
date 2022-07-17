b2b76345ca9c3a1c676120665b36c69124eba670
Jason Ceballos <30805605+jasonceballos29@users.noreply.github.com>
2022-07-16 21:37:36 -0400

    Removing old terraform lock file

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -8,9 +8,7 @@ module "vpc" { #A
   azs                              = data.aws_availability_zones.available.names
   private_subnets                  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets                   = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  database_subnets                 = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
-  
-  create_database_subnet_group     = true
+ 
   enable_nat_gateway               = true
   single_nat_gateway               = true
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,2 @@
-namespace = "my-3-tier-architecture"
-region    = "us-west-2"
+namespace = "tfproject-3-tier-architecture"
+region    = "us-east-1"
