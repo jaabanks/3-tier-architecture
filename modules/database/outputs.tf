ee26f340f495e5db311c33645e0bb2b5f8c41081
Jason Ceballos <30805605+jasonceballos29@users.noreply.github.com>
2022-07-16 21:41:44 -0400

    Removing DB references

--- modules/database/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "db_config" {
-  value = {
-    user     = aws_db_instance.database.username #A
-    password = aws_db_instance.database.password #A
-    database = aws_db_instance.database.name #A
-    hostname = aws_db_instance.database.address #A
-    port     = aws_db_instance.database.port #A
-  }
-}
