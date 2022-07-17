a2f46796af2edde825fd5d605ad900c3746ec28c
Jason Ceballos <30805605+jasonceballos29@users.noreply.github.com>
2022-07-16 21:41:36 -0400

    Removing db references

--- main.tf
+++ main.tf
@@ -8,16 +8,6 @@ module "autoscaling" {
   db_config = module.database.db_config #A
 
 }
-
-module "database" {
-  source    = "./modules/database" #A
-  namespace = var.namespace        #B
-
-  vpc = module.networking.vpc #A
-  sg  = module.networking.sg  #A
-
-}
-
 module "networking" {
   source    = "./modules/networking" #A
   namespace = var.namespace          #B
--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "db_password" {
-  value = module.database.db_config.password
-  sensitive = true
-}
-
 output "lb_dns_name" {
   value = module.autoscaling.lb_dns_name
 }
